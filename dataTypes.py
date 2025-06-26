d992e3efa2518067521e5a10a81b847f1ee17359
Jade <jade.vu.nguyen@gmail.com>
2025-06-25 23:01:34 -0700

    Converting data types

---
+++ dataTypes.py
@@ -0,0 +1,26 @@
+
+#create variables int and float 
+num1 = 10 
+num2 = 3.14
+
+#printing the type 
+print(type(num1))
+print(type(num2))
+
+#converting to integer and float to string
+num_str = str(num1)
+print("Number as string: " + num_str)
+
+num_str2 = str(num2)
+print("Number as String: " + num_str2)
+
+#converting string into and adding 10
+str_num = "123"
+num3 = int(str_num) + 10
+print(num3)
+
+# checking and converting bools 0 = false and 1 = true
+
+print(bool(0))
+print(bool(1))
+print(bool("False"))
