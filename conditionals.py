d4f847a0d03b7e51bfe74f15442aeed8e5704eaf
Jade <jade.vu.nguyen@gmail.com>
2025-06-26 14:14:13 -0700

    conditional practice

---
+++ conditionals.py
@@ -0,0 +1,12 @@
+#Get user to give a number between 0-100 and if pass x >  60 otherwise fail
+
+grade = int(input("What is your grade?:  "))
+
+#elif statements, check if grade is over 60 or else print "Fail"
+
+if grade >= 60 :
+    print("Pass!")
+else:
+    print("Fail")
+
+
