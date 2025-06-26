dd9b88080c7f34180725f831a04288408bf538d7
Jade <jade.vu.nguyen@gmail.com>
2025-06-25 22:40:09 -0700

    Remove basics.py from old repo

--- basics.py
+++
@@ -1,8 +0,0 @@
-#Create variables
-
-name = "Button"
-age = 22
-
-#print variables in formatted structure vars in {} replaced with int and string assigned
-print(f"My name is {name} and I am {age} years old.")
-
