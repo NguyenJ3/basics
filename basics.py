845e6f02396266dc9250c16e9c8feac41b75586b
Jade <jade.vu.nguyen@gmail.com>
2025-06-25 22:45:50 -0700

    Initial commit

---
+++ basics.py
@@ -0,0 +1,8 @@
+#Create variables
+
+name = "Button"
+age = 22
+
+#print variables in formatted structure vars in {} replaced with int and string assigned
+print(f"My name is {name} and I am {age} years old.")
+
