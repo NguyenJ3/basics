c7cbfb4f90a889c33a3e7a214bf0b37100a347fb
Jade <jade.vu.nguyen@gmail.com>
2025-06-26 19:39:18 -0700

    Final commit added more conditional loops

--- pin.py
+++ pin.py
@@ -1,41 +1,37 @@
-
+#number of tries 
+tries =  0 
 #userinput must match the following pin 
 
 correctPin =  "3014489"
 
 enteredPin = (input("Enter your pin!"))
 
-confirmation =  ("Yes").lower()
+tries += 1
+
+confirmation =  "Yes"
 
 
+#checks if enterpin is not correctpin then prints that is not correct 
 if enteredPin != correctPin:
 
     print("That is not correct")
+#if the pin isnt correct prints out a hint message that converts text to lower 
+    decision = (input("Would you like a hint ").lower())
 
-    decision = print(input("Would you like a hint ").lower())
-
-    if decision == confirmation:
+    if decision == confirmation.lower():
        print("Okay give a moment")
-        
+
+    #checks length of string and compares whether or not pin is the same length     
     if len(correctPin) == len(enteredPin):
       print("They are the same length!")
 
 
     else: 
        print("The lenght of pin is wrong!")
-            
-        
-        
-        #decision == (confirmation):
-           # len(correctPin) == len(enteredPin)
-           # print("length of password is correct")
-         
-        #else:
-            #print("it is the wrong length")
 
 
-#else:
-   #print("Success!!")
+else:
+   print("Success!!")
 
 
 
